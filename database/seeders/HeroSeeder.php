e2d8b1743dc4642ec2ce08d4736b92b5e72ed4b3
skonry <skonry1604@gmail.com>
2021-09-19 18:28:16 +0200

    Updated data files and modified .gitignore files for storage files

--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -19,7 +19,7 @@ class HeroSeeder extends Seeder
         'artanis', 'arthas', 'auriel', 'azmodan', 'blaze', 'brightwing', 'cassia',
         'chen', 'chogall', 'chromie', 'deathwing', 'deckard', 'dehaka', 'diablo',
         'dva', 'etc', 'falstad', 'fenix', 'gall', 'garrosh', 'gazlowe', 'genji',
-        'greymane', 'guldan', 'hanzo', 'illidan', 'imperius', 'jaina', 'johanna',
+        'greymane', 'guldan', 'hanzo', 'hogger', 'illidan', 'imperius', 'jaina', 'johanna',
         'junkrat', 'kaelthas', 'kelthuzad', 'kerrigan', 'kharazim', 'leoric',
         'lili', 'liming', 'lostvikings', 'ltmorales', 'lucio', 'lunara', 'maiev',
         'malfurion', 'malganis', 'malthael', 'medivh', 'mephisto', 'muradin', 'murky',
@@ -40,7 +40,7 @@ class HeroSeeder extends Seeder
     }
     private function loadJsonData($fileName)
     {
-        $json = Storage::disk('public')->get('heroes_data/' . $fileName . '.json');
+        $json = Storage::disk('public')->get('hero/' . $fileName . '.json');
         $data = json_decode($json);
         return $data;
     }
--- database/seeders/TalentSeeder.php
+++ database/seeders/TalentSeeder.php
@@ -20,7 +20,7 @@ class TalentSeeder extends Seeder
         'artanis', 'arthas', 'auriel', 'azmodan', 'blaze', 'brightwing', 'cassia',
         'chen', 'chogall', 'chromie', 'deathwing', 'deckard', 'dehaka', 'diablo',
         'dva', 'etc', 'falstad', 'fenix', 'gall', 'garrosh', 'gazlowe', 'genji',
-        'greymane', 'guldan', 'hanzo', 'illidan', 'imperius', 'jaina', 'johanna',
+        'greymane', 'guldan', 'hanzo', 'hogger', 'illidan', 'imperius', 'jaina', 'johanna',
         'junkrat', 'kaelthas', 'kelthuzad', 'kerrigan', 'kharazim', 'leoric',
         'lili', 'liming', 'lostvikings', 'ltmorales', 'lucio', 'lunara', 'maiev',
         'malfurion', 'malganis', 'malthael', 'medivh', 'mephisto', 'muradin', 'murky',
@@ -54,7 +54,7 @@ class TalentSeeder extends Seeder
     }
     private function loadJsonData($fileName)
     {
-        $json = Storage::disk('public')->get('heroes_data/' . $fileName . '.json');
+        $json = Storage::disk('public')->get('hero/' . $fileName . '.json');
         $data = json_decode($json);
         return $data;
     }
